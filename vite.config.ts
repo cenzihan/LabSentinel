b73278f3fb05e217684b592eb997aaae0c65ae76
Alvin <alvin.gg@qq.com>
2026-03-29 12:10:42 +0800

    Add realtime detection

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const port = parseInt(env.VITE_PORT || '5173', 10)
   const serverPort = env.SERVER_PORT || '3001'
+  const useHostAndSsl = env.VITE_HOST_AND_SSL === 'true'
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      useHostAndSsl ? basicSsl() : null
+    ].filter(Boolean),
     server: {
       port,
+      host: useHostAndSsl,
       proxy: {
         '/api': {
           target: `http://localhost:${serverPort}`,
